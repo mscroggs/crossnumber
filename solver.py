4301eabeb981947c14ef475603f5228a38239f1c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-02-05 10:44:15 +0000

    added code for descs

--- solver.py
+++ solver.py
@@ -16,15 +16,22 @@ class Solver:
     def __init__(self, grid):
         self.grid = grid
         self.options = []
+        self.clue_desc = {}
 
-    def set_clue(self, *args, **kwargs):
-        self.grid.set_clue(*args, **kwargs)
+    def set_clue(self, clue, value, desc=None):
+        self.grid.set_clue(clue, value)
+        self.clue_desc[clue] = desc
 
-    def set_clue_options(self, clues, options):
+    def set_clue_options(self, clues, options, descs=None):
         if isinstance(clues, str):
-            self.options.append(([clues],[[i] for i in options]))
+            self.options.append(([clues],[[i] for i in options],[descs]))
         else:
-            self.options.append((clues, options))
+            if descs is None:
+                descs = [None for i in clues]
+            self.options.append((clues, options, descs))
 
     def find_solutions(self):
         print_all(self.grid, *self.options)
+
+    def as_latex(self):
+        return self.grid.as_latex()
